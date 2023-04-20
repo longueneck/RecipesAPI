5bf1dbb538f23b84dd8e8a8f329b15527b4eb10f
longueneck <kevinlongue@hotmail.com>
2023-04-20 11:19:35 -0300

    buf fix

--- API/app.js
+++ API/app.js
@@ -1,29 +1,52 @@
 const express = require("express");
 const { randomUUID } = require("crypto");
+const fs = require("fs");
+
+
+function getID() {
+  let id = 0;
+ return function(){
+  id++;
+  return id;
+ };
+}
 
 const app = express();
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    products = JSON.parse(data);
+  }
+});
 
 app.post("/products", (request, response) => {
- 
-  const { name, price } = request.body;
+  const { name, price, id } = request.body;
 
-  console.log(request.body)
+  console.log(request.body);
 
-  const product = 
-
-  {
+  const product = {
     name,
     price,
-    id: randomUUID(),
+    id : getID(),
   };
 
   products.push(product);
 
- return response.json(product);
+  fs.writeFile("products.json", JSON.stringify(products), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Produto Inserido");
+    }
+  });
+
+  return response.json(product);
 });
 
 app.get("/products", (request, response) => {
@@ -31,9 +54,14 @@ app.get("/products", (request, response) => {
 });
 
 app.get("/products/:id", (request, response) => {
-  const {id} = request.params;
-  const product = products.find(products => product.id === id)
-})
+  const { id } = request.params;
+  const product = products.find((products) => product.id === id);
+  return response.json(product);
+});
 
+app.put("/products/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, price } = request.body;
+});
 
 app.listen(4002, () => console.log("Servidor esta rodando na porta 4002"));
