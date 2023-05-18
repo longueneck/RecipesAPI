aebcad480317a0bb6089c52299b050fb9dd726fc
longueneck <kevinlongue@hotmail.com>
2023-05-18 13:23:50 -0300

    [FIX]

--- API/recipes.js
+++ API/recipes.js
@@ -15,23 +15,8 @@ fs.readFile("recipes.json", "utf-8", (err, data) => {
   }
 });
 
-// Pegar todos os elementos
 app.get("/recipes", (request, response) => {
   return response.json(recipes);
-});
-
-// Pegar elementos pelo indice
-app.get("/recipes/:id", (request, response) => {
-  const { id } = request.params;
-  const recipe = recipes.find((recipe) => recipe.id == id);
-  return response.json(recipe);
-});
-
-// Pegar elementos pelo nome
-app.get("/recipes/:name", (request, response) => { // Renomear para evitar conflitos
-  const { name } = request.params;
-  const recipe = recipes.filter((recipe) => recipe.name == name);
-  return response.json(recipe);
 });
 
 app.get('/recipes/ingredients', (req, res) => {
@@ -39,17 +24,17 @@ app.get('/recipes/ingredients', (req, res) => {
 
   const filteredRecipes = recipes.filter(recipe => {
     const recipeIngredients = recipe.ingredients;
-
+    console.log(recipe.ingredients);
     return ingredients.every(ingredient =>
-      recipeIngredients.includes(ingredient)
+      recipeIngredients.indexOf(ingredient) > -1   
     );
   });
 
   if (filteredRecipes.length === 0) {
     return res.status(404).json({ message: "Nenhuma receita encontrada" });
   }
-
-  return res.json(filteredRecipes[0]);
+  
+  return res.json(filteredRecipes);
 });
 
 
